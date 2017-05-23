9738545742adf281f940e50948a1fcb1fc1a5304
Rohan <rbalel@purdue.edu>
2017-05-23 15:39:19 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 class App extends Component {
@@ -6,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-         
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
