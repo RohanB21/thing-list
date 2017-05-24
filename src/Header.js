8c27c58ab7f0f08e5bffc50e72ff35878a966e15
Rohan <rbalel@purdue.edu>
2017-05-24 14:50:26 -0400

    Update till First Break

--- src/Header.js
+++ src/Header.js
@@ -9,17 +9,6 @@ const Header = () => {
     <header>
       <h1>ThingList</h1>
       <h2>So Many Things</h2>
-      <form>
-    <label>
-      Name:
-     <input type="text" name="name" />
-     </label>
-     <button className="Add">
-            <i className="fa fa-plus"></i>
-          </button>
-
-</form>
-
       
     </header>
   )
